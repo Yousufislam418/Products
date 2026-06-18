8d4551354650a435234d100dfb9893cd8ee656d1
Yousufislam418 <yousufislamaps@gmail.com>
2026-06-18 22:00:18 +0300

    first commit

--- components/products-data.js
+++ components/products-data.js
@@ -5,8 +5,30 @@ const products = [
         "barcode": "6308338002",
         "partnerSku": "063-08-3380-02",
         "sku": "Z571F1249538DDBBC99A7Z-1",
+        "description": "null",
+        "packing": "80 number Plastic",
         "image":"../images/Laundry basket with Lid.jpg"
-    }
+    },
+    {
+        "product-id":"2",
+        "product_name": "3-Tier Shoe Rack Organizer Beige 55.5x21.7x61 cm 9 Paris of Shoe concept_home",
+        "barcode": "65170842031",
+        "partnerSku": "046-08-3555B-03",
+        "sku": "ZFCC4A1B3F20A426977D3Z-1",
+        "description": "null",
+        "packing": "null",
+        "image":"../images/3-Tier Shoe Rack.jpg"
+    },
+    // {
+    //     "product-id":"1",
+    //     "product_name": "",
+    //     "barcode": "",
+    //     "partnerSku": "",
+    //     "sku": "",
+    //     "description": "",
+    //     "packing": "",
+    //     "image":""
+    // }
 ];
 
 export default products;
--- components/products-display.js
+++ components/products-display.js
@@ -1,24 +1,31 @@
 import products from "./products-data.js";
 
+
 export default function productsDisplay(){
-
- products.map((product)=>{
-   const productName = product.product_name;
-
-     });
-
- 
 
  return(`
     <section>
-     <div class="container">
-      <div class="content">
-       <h1>Product name : ${productName}</h1>
-       <img class="w-300" src="../images/Laundry basket with Lid.jpg">
-      </div>
+     <div class="container w-100-pr mx-auto" id="product-display">
+    ${
+      products.map((product,index)=> `
+      <div class="content m-10 shadow-10 p-10 radius-10">
+       <h3 class="mb-10">${product.product_name}</h3>
+       <img class="w-100-pr radius-5" src="${product.image}">
+       <p class="p-10 m-3 radius-3 border-1 -sky-1">${product.barcode}</p>
+       <p class="p-10 m-3 radius-3 border-1 -sky-1">${product.partnerSku}</p>
+       <p class="p-10 m-3 radius-3 border-1 -sky-1">${product.sku}</p>
+       <p class="p-10 m-3 radius-3 border-1 -sky-1">${product.packing}</p>
+       <p class="p-10 m-3 radius-3 border-1 -sky-1">${product.description}</p>
+      </div>`).join('')
+    }
      </div>
     </section>
   `);
 
 
 }
+
+    // <div class="content">
+    //    <h1>Product name : ${productName ? productName : ''}</h1>
+    //    <img class="w-300" src="${image}">
+    //   </div>
